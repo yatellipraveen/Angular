e19cf8ac4a0cc9295752dfd045d5bda70652b9b6
yatellipraveen <yatellipraveen@gmail.com>
2020-04-13 13:07:18 +0530

    removed unused files and imports

--- user-management/src/app/shared/users-list/userslist.component.ts
+++ user-management/src/app/shared/users-list/userslist.component.ts
@@ -1,24 +1,20 @@
-import { Component, Input, OnInit, OnChanges, DoCheck } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { User } from 'src/app/user.model';
 import { HttpService } from 'src/app/http.service';
 
 @Component({
   selector: 'app-userslist',
-  templateUrl: './userslist.component.html'
+  templateUrl: './userslist.component.html',
 })
-export class UserslistComponent implements OnInit, DoCheck {
+export class UserslistComponent implements OnInit {
   @Input() module: string;
   userList: User[];
-  constructor( private httpService: HttpService) {}
+  constructor(private httpService: HttpService) {}
 
   ngOnInit() {
-    this.httpService.getUsers().subscribe(data => {
+    this.httpService.getUsers().subscribe((data) => {
       this.userList = data as any;
     });
-  }
-
-  ngDoCheck() {
-   
   }
 }
